c3b5fb81e48caf5cd73ca4d3fc963ad72b1a25ad
gersonraniery <77862175+gersonraniery@users.noreply.github.com>
2022-03-21 13:08:41 -0300

    Update jogo da advinhacao.py

--- jogo da advinhacao.py
+++ jogo da advinhacao.py
@@ -4,11 +4,15 @@ from time import sleep
 print('-=-'*20)
 print('vou pensar em um número de 0 a 5. Tente advinhar...')
 print('-=-'*20)
-computador = randint(0, 5)
-resp = int(input('Em que número pense? '))
+
+# computador escolhendo um número
+num_computador = randint(0, 5)
+
+resp = int(input('Em que número pensei? '))
 print('PROCESSANDO...')
 sleep(1)
-if resp == computador:
+
+if resp == num_computador:
     print('PARABÉNS!Você conseguiu me vencer!')
 else:
-    print(f'GANHEI! Eu pensei no número {computador}.')
+    print(f'GANHEI! Eu pensei no número {num_computador}.')
