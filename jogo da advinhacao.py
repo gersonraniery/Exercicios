911ba45b8d41bba99e1c910e9204b92787aba9d1
gersonraniery <77862175+gersonraniery@users.noreply.github.com>
2022-03-21 13:06:55 -0300

    Add files via upload

---
+++ jogo da advinhacao.py
@@ -0,0 +1,14 @@
+from random import randint
+from time import sleep
+
+print('-=-'*20)
+print('vou pensar em um número de 0 a 5. Tente advinhar...')
+print('-=-'*20)
+computador = randint(0, 5)
+resp = int(input('Em que número pense? '))
+print('PROCESSANDO...')
+sleep(1)
+if resp == computador:
+    print('PARABÉNS!Você conseguiu me vencer!')
+else:
+    print(f'GANHEI! Eu pensei no número {computador}.')
